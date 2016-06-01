899c48de26d1ee89781c059b50d0d87d1eaf3363
Habib <habibhoxha@yahoo.com>
2016-06-01 11:55:10 +0200

    Init

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	
+	// Navigation click
+	$("#navbar li a").click(function(){
+		var page = $(this).data("show");
+		console.log(page);
+		$(".section").hide();
+		$("#"+page).show();
+		
+		//$(".section").fadeOut().promise().done(function(){
+		//	$("#"+page).fadeIn();
+		//});
+		
+	});
+	
+	$(".phone").click(function(){
+		var title = $(this).find(".phone_title").html();
+		var manufacturer = $(this).find(".phone_manufacturer span").html();
+		var os = $(this).find(".phone_os").html();
+		var storage = $(this).find(".phone_storage").html();
+		var camera = $(this).find(".phone_camera").html();
+		var image = $(this).find(".phone_thumbnail img").data("large-image");
+		
+		
+		$("#phone_details").find(".phone_manufacturer span").html(manufacturer);
+		$("#phone_details").find(".phone_os").html(os);
+		$("#phone_details").find(".phone_storage").html(storage);
+		$("#phone_details").find(".phone_camera").html(camera);
+		$("#phone_details").find(".phone_thumbnail img").attr("src", image);
+		
+		$("#overlay").animate({
+			height: "100%"
+		});
+	});
+	
+	$("#overlay").click(function(){
+		$(this).animate({
+			height: "0"
+		});
+	});
+});
