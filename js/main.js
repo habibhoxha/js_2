609c4859c8d36a90677ae538ab2b95b568f33e7e
Habib <habibhoxha@yahoo.com>
2016-06-06 12:23:27 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 		var camera = $(this).find(".phone_camera").html();
 		var image = $(this).find(".phone_thumbnail img").data("large-image");
 		
-		
+		$("#phone_details").find(".phone_title").html(title);
 		$("#phone_details").find(".phone_manufacturer span").html(manufacturer);
 		$("#phone_details").find(".phone_os").html(os);
 		$("#phone_details").find(".phone_storage").html(storage);
@@ -38,4 +38,16 @@ $(document).ready(function(){
 			height: "0"
 		});
 	});
+	
+	
+	$.getJSON("json/phones.json", function(data){
+		var phones = data.phones;
+		console.log(data.phones);
+		
+		for(var i=0; i<phones.length; i++) {
+			var phone = phones[i];
+			console.log(phone.name);
+		}
+	});
+	
 });
